b090ee30b53e9a9eeb62f8cf666aee40e5e55350
N10ai <infon10miami@gmail.com>
2026-07-16 07:27:00 -0400

    Enable global portal and drawer interaction fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import { AppEnhancements } from './components/AppEnhancements'
 import './design-system/tokens.css'
 import './design-system/components.css'
 import './styles.css'
@@ -9,10 +10,12 @@ import './premium.css'
 import './request-workspace.css'
 import './vendor-workspace.css'
 import './public-portal.css'
+import './stability-fixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
+      <AppEnhancements />
       <App />
     </HashRouter>
   </React.StrictMode>,
