2b3094c829c5893b1c9f66f645b4c21f26eaf734
N10ai <infon10miami@gmail.com>
2026-07-16 15:23:13 -0400

    Load final UI stabilization last

--- src/main.tsx
+++ src/main.tsx
@@ -29,6 +29,7 @@ import './quote-output-and-rate-basis.css'
 import './pricing-usability-fixes.css'
 import './quote-mobile-workspace.css'
 import './controlled-pricing-and-print.css'
+import './final-ui-stabilization.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
