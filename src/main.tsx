81c3be71aad74ef63578f6325a1f841290f47d5d
N10ai <infon10miami@gmail.com>
2026-07-17 18:38:53 -0400

    Load dedicated mobile quote styles

--- src/main.tsx
+++ src/main.tsx
@@ -33,6 +33,7 @@ import './final-ui-stabilization.css'
 import './universal-selector.css'
 import './mobile-stability-final.css'
 import './mobile-system-v2.css'
+import './mobile-quote-app.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
