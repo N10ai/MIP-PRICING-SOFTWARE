724b5d4369cabd7990a7b7dfbe81c6a8da2bf528
N10ai <infon10miami@gmail.com>
2026-07-25 20:23:55 -0400

    feat: mount reliable PDF generator and readable mobile summary

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { MobileAccountDrawerEnhancer } from './components/MobileAccountDrawerEnh
 import { MobileQueueControls } from './components/MobileQueueControls'
 import { MobileRequestNavigation } from './components/MobileRequestNavigation'
 import { MobileQuoteSwipeEnhancer } from './components/MobileQuoteSwipeEnhancer'
+import { MobileQuotePdfEnhancer } from './components/MobileQuotePdfEnhancer'
 import { RfqQuickActions } from './components/RfqQuickActions'
 import { DesktopWorkspaceQueue } from './components/DesktopWorkspaceQueue'
 import { DesktopCommercialEnhancements } from './components/DesktopCommercialEnhancements'
@@ -87,6 +88,7 @@ import './apple-quote-workspace-v10.css'
 import './mobile-quote-apple-v2.css'
 import './mobile-quote-apple-v12.css'
 import './professional-quote-document-v3.css'
+import './mobile-quote-summary-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -98,6 +100,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <MobileQueueControls />
         <MobileRequestNavigation />
         <MobileQuoteSwipeEnhancer />
+        <MobileQuotePdfEnhancer />
         <RfqQuickActions />
         <DesktopWorkspaceQueue />
         <DesktopCommercialEnhancements />
